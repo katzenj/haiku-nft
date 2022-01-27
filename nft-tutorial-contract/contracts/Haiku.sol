cbd939d6e3c75226e1bf2b7e86a94a51ad2171af
Jordan Katzen <jkatzen97@gmail.com>
2022-01-27 12:49:36 -0800

    more changes

--- nft-tutorial-contract/contracts/Haiku.sol
+++ nft-tutorial-contract/contracts/Haiku.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
 
 // [MIT License]
 /// @title Base64
@@ -93,7 +94,7 @@ contract Haiku is ERC721Enumerable, ERC721URIStorage, Ownable  {
 
   // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
   // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='#55849f' /><text x='50%' y='25%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  string private constant baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='#55849f' /><text x='50%' y='25%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   event NFTMinted(address indexed to, uint256 timestamp, uint256 tokenId);
   event HaikuUpdated(address indexed owner, uint256 timestamp, uint256 tokenId);
@@ -109,26 +110,6 @@ contract Haiku is ERC721Enumerable, ERC721URIStorage, Ownable  {
     require(
         _isApprovedOrOwner(msg.sender, tokenId),
         "You can't edit a poem you don't own..."
-    );
-    _;
-  }
-
-  modifier validHaikuLength(uint256 haikuLength) {
-    require(
-      haikuLength == 3,
-      "Haikus are 3 lines"
-    );
-    _;
-  }
-
-  modifier validHaiku(string[] calldata haiku) {
-    require(
-      haiku.length == 3,
-      "haikus are 3 lines"
-    );
-    require(
-      bytes(haiku[0]).length < 40 && bytes(haiku[1]).length < 40 && bytes(haiku[2]).length < 40,
-      "Haiku size is limited."
     );
     _;
   }
@@ -196,30 +177,27 @@ contract Haiku is ERC721Enumerable, ERC721URIStorage, Ownable  {
     emit NFTMinted(msg.sender, block.timestamp, newItemId);
   }
 
-  function updatePoem(uint256 tokenId, string[] calldata haiku)
+  function updatePoem(
+    uint256 tokenId,
+    string calldata bgColor,
+    string calldata fontColor
+  )
     external
     ownsToken(tokenId)
-    validHaiku(haiku)
   {
-    string memory svgText = string(abi.encodePacked(
-      '<tspan x="50%" dy="0em">',
-      haiku[0],
-      '</tspan><tspan x="50%" dy="1.1em">',
-      haiku[1],
-      '</tspan><tspan x="50%" dy="1.1em">',
-      haiku[2],
-      '</tspan>'
-    ));
-    bytes memory finalSvg = abi.encodePacked(baseSvg, svgText,"</text></svg>");
-    string memory json = Base64.encode(
-      abi.encodePacked(_getJsonBase(tokenId), Base64.encode(finalSvg), '"}')
-    );
-    string memory finalTokenUri = string(
-        abi.encodePacked("data:application/json;base64,", json)
-    );
+    string memory tokenUri = tokenURI(tokenId);
+    console.log(tokenUri);
+  // string private constant baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='#55849f' /><text x='50%' y='25%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  //   bytes memory finalSvg = abi.encodePacked(baseSvg, svgText,"</text></svg>");
+  //   string memory json = Base64.encode(
+  //     abi.encodePacked(_getJsonBase(tokenId), Base64.encode(finalSvg), '"}')
+  //   );
+  //   string memory finalTokenUri = string(
+  //       abi.encodePacked("data:application/json;base64,", json)
+  //   );
 
-    _setTokenURI(tokenId, finalTokenUri);
-    emit HaikuUpdated(msg.sender, block.timestamp, tokenId);
+  //   _setTokenURI(tokenId, finalTokenUri);
+  //   emit HaikuUpdated(msg.sender, block.timestamp, tokenId);
   }
 
   // ============ PRIVATE ADMIN-ONLY FUNCTIONS ============
