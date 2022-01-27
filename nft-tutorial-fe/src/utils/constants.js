de0ac21bd4d6b90fd0c1b5751c0f99771ddf02f7
Jordan Katzen <jkatzen97@gmail.com>
2022-01-27 13:07:13 -0800

    remove unnecessary things

--- nft-tutorial-fe/src/utils/constants.js
+++ nft-tutorial-fe/src/utils/constants.js
@@ -1,7 +1,4 @@
 export const INFURA_ID = "e69606221186406c9b3633ac3cb7e3ea";
-// export const ALCHEMY_KEY = "UCS1l1rFW4wTZbmky4IAZ1H9rqiKByE_"
 export const ALCHEMY_KEY = "T49Q5azAGAqODl2d1EP7Izu5JDwnPE4S"
-// export const CHAIN_ID = "4";
-export const CHAIN_ID = "80001";
-// export const CONTRACT_ADDRESS = "0xE723428534433E3D187E6e04F2f00C747f1A8160";
-export const CONTRACT_ADDRESS = "0x1a97B29f61A6f4F014baAb29CF9ad911Bdd656DF";
+export const CHAIN_ID = "80001";  // mumbai
+export const CONTRACT_ADDRESS = "0x1a97B29f61A6f4F014baAb29CF9ad911Bdd656DF";  // mumbai
